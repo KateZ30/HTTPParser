10d1b053de9928f40f2380981e85d74c571445c4
Helge Hess <helge@alwaysrightinstitute.com>
2016-04-27 15:27:07 +0200

    Finish up test.

--- Tests/HTTPParserTests.swift
+++ Tests/HTTPParserTests.swift
@@ -17,44 +17,64 @@ class HTTPParserTests: XCTestCase {
     "Content-type: text/plain\r\n" +
     "\r\n";
 
+  let debugLog = false
+  
   func testSimpleGetRequest() {
-    let parser = HTTPParser(type: .HTTP_REQUEST)
+    let debugLog = self.debugLog // capture
+    
+    let parser = HTTPParser(type: .Request)
     XCTAssertNotNil(parser)
     
+    // NOTE: The approach to collect headers shown here does NOT WORK for
+    //       sources pushing in chunks of data
+    var lastHeaderField : String? = nil
+    var headers = Dictionary<String, String>()
+    
     parser.onMessageBegin { p in
-      print("*** CB: message begin")
+      if debugLog { print("*** CB: message begin") }
       return 0
     }
     parser.onMessageComplete { p in
-      print("*** CB: message DONE")
+      if debugLog { print("*** CB: message DONE") }
       return 0
     }
     parser.onHeadersComplete { p in
-      print("*** CB: headers done: method=\(p.method)")
+      if debugLog { print("*** CB: headers done: method=\(p.method)") }
+      XCTAssertNotNil(p.method)
+      XCTAssertEqual(p.method, HTTPMethod.GET)
+      XCTAssertEqual(p.http_major!, 1)
+      XCTAssertEqual(p.http_minor!, 1)
       return 0
     }
     parser.onHeaderField { p, ptr, len in
       let s = String.fromCString(ptr, length: len)
-      print("*** CB: header field: \(s)")
+      if debugLog { print("*** CB: header field: \(s)") }
+      lastHeaderField = s
       return 0
     }
     parser.onHeaderValue { p, ptr, len in
       let s = String.fromCString(ptr, length: len)
-      print("*** CB: header value: \(s)")
+      if debugLog { print("*** CB: header value: \(s)") }
+      XCTAssert(s != nil)
+      XCTAssert(lastHeaderField != nil)
+      headers[lastHeaderField!] = s!
       return 0
     }
     parser.onBody { p, ptr, len in
-      print("*** CB: body")
+      if debugLog { print("*** CB: body") }
+      XCTAssertEqual(0,len) // no body in request
       return 0
     }
     parser.onStatus { p, ptr, len in
       let s = String.fromCString(ptr, length: len)
-      print("*** CB: status \(s)")
+      if debugLog { print("*** CB: status \(s)") }
+      XCTAssert(false) // should not be called on request ...
       return 0
     }
     parser.onURL { p, ptr, len in
       let s = String.fromCString(ptr, length: len)
-      print("*** CB: url: \(s)")
+      if debugLog { print("*** CB: url: \(s)") }
+      XCTAssertEqual(s!, "/index.html")
       return 0
     }
     
@@ -68,6 +88,13 @@ class HTTPParserTests: XCTestCase {
     // send EOF
     let nb  = parser.execute(nil, 0)
     XCTAssertEqual(nb, 0)
+    
+    
+    // finish up
+    if debugLog { print("HEADERS: \(headers)") }
+    XCTAssert(headers.count == 2)
+    XCTAssertEqual(headers["Content-type"],   "text/plain")
+    XCTAssertEqual(headers["Content-length"], "0")
   }
   
 }
